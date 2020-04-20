e3c02ede1d332ad96bddeaaf78c5ab68ed59a47f
MohanadDahi <mohanad.13.93@gmail.com>
2020-04-20 13:02:37 +0200

    apply shuffel script on images

--- js/main.js
+++ js/main.js
@@ -9,4 +9,21 @@ $(function(){
 
     $('.slider,.carousel-inner').height(windowHeight - (navHeight+upperHeight))
 
+
+    // Featured work shffel
+    // when I click on li 
+    $('.featuredwork .list-unstyled ul li').on('click',function()
+    {
+        // add class 'active' on the pressed li and remove 'active' class from the siblings
+        $(this).addClass('active').siblings().removeClass('active');
+        if($(this).data('class') === 'all'){
+            $('.shuffel .col-sm').css('opacity','1')           
+        }
+        else
+        {
+            $('.shuffel .col-sm').css('opacity','0.09') ;
+            $($(this).data('class')).parent().css('opacity','1')   
+        }
+    })
+
 })
